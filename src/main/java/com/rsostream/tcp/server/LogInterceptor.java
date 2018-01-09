dd47fa885d2e79b637f852cb1bab8d628500dc73
Marko <markoprelevik@gmail.com>
2018-01-10 00:07:55 +0100

    added /metrics endpoint, enabled logging

---
+++ src/main/java/com/rsostream/tcp/server/LogInterceptor.java
@@ -0,0 +1,38 @@
+package com.rsostream.tcp.server;
+
+import com.kumuluz.ee.common.config.EeConfig;
+import com.kumuluz.ee.common.runtime.EeRuntime;
+import com.kumuluz.ee.logs.cdi.Log;
+import org.apache.logging.log4j.CloseableThreadContext;
+
+import javax.annotation.Priority;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.util.HashMap;
+import java.util.UUID;
+
+@Log
+@Interceptor
+@Priority(Interceptor.Priority.PLATFORM_BEFORE)
+public class LogInterceptor {
+
+    @AroundInvoke
+    public Object logMethodEntryAndExit(InvocationContext context) throws Exception {
+
+        HashMap settings = new HashMap();
+
+        settings.put("environment", EeConfig.getInstance().getEnv().getName());
+        settings.put("serviceName", EeConfig.getInstance().getName());
+        settings.put("applicationVersion", EeConfig.getInstance().getVersion());
+        settings.put("uniqueInstanceId", EeRuntime.getInstance().getInstanceId());
+
+        settings.put("uniqueRequestId", UUID.randomUUID().toString());
+
+        try (final CloseableThreadContext.Instance ctc = CloseableThreadContext.putAll(settings)) {
+            Object result = context.proceed();
+            return result;
+        }
+    }
+
+}
